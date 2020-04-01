7b44fda6f74444e8d8772ba84ddec48b8930be19
Kent Kobi <kentkobi@hotmail.com>
2020-04-01 18:42:53 +1100

    roughed in some styling and functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Counter() {
-  return <div>- 8 +</div>;
+export default function Counter( props ) {
+	const { id, label } = props.counter;
+	const [quantity, setQuantity] = useState(props.counter.quantity);
+
+	return (
+		<div className="counter">
+			<div className="counter-label">
+				{label}
+			</div>
+			<div className="counter-quantity">
+				<button onClick={() => setQuantity(quantity - 1)}>-</button>
+				<span>{quantity}</span>
+				<button onClick={() => setQuantity(quantity + 1)}>+</button>
+			</div>
+		</div>
+	);
 }
--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Total() {
-  return <div>Total 10</div>;
+export default function Total( props ) {
+	const total = props.counters.reduce((sum, counter) => sum + counter.quantity, 0);
+  return (
+  	<div class="total">
+  		<div class="total-label">Total</div>
+  		<div class="total-quantity">{total}</div>
+  	</div>
+  ) 	
 }
